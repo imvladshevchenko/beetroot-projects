61fb1787e3507eaac6c31224c7e8e378cca80caf
Vladyslav Shevchenko <imvladshevchenko@gmail.com>
2020-05-15 16:09:59 +0300

    fixed app.js

--- projects/cahee/src/js/app.js
+++ projects/cahee/src/js/app.js
@@ -3,9 +3,7 @@ const navBar = document.getElementById("navBar");
 document.addEventListener("scroll", () => {
     if (window.pageYOffset < navBar.clientHeight) {
       navBar.classList.remove("filled");
-      toTop.classList.remove("filled");
     } else {
       navBar.classList.add("filled");
-      toTop.classList.add("filled");
     }
   });
