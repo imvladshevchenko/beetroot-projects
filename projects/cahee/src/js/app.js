29dadf07481cd41f553b67f3c033ab1de3566c9e
Vladyslav Shevchenko <imvladshevchenko@gmail.com>
2020-06-20 15:57:28 +0300

    added exam

--- projects/cahee/src/js/app.js
+++ projects/cahee/src/js/app.js
@@ -14,3 +14,10 @@ document.addEventListener("scroll", () => {
     mobileMenu.classList.toggle("active");
   });
 
+  function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+      mapId: "8e0a97af9386fef",
+      center: { lat: 48.85, lng: 2.35 },
+      zoom: 12
+    });
+  }
