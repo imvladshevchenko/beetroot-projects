f6840de74e37c6f4b15773e4431a771581a7d488
Vladyslav Shevchenko <imvladshevchenko@gmail.com>
2020-07-28 02:00:16 +0300

    completed apavtive layout

--- projects/cahee/src/js/app.js
+++ projects/cahee/src/js/app.js
@@ -5,14 +5,23 @@ const mobileMenu = document.getElementById("mobileNav");
 document.addEventListener("scroll", () => {
     if (window.pageYOffset < navBar.clientHeight) {
       navBar.classList.remove("filled");
+      burger.classList.remove("filled");
     } else {
       navBar.classList.add("filled");
+      burger.classList.add("filled");
     }
   });
 
-  burger.addEventListener("click", () => {
-    mobileMenu.classList.toggle("active");
-  });
+  // document.addEventListener("scroll", () => {
+  //   if (window.pageYOffset < burger.clientHeight) {
+  //     burger.classList.remove("filled");
+  //   } else {
+  //     burger.classList.add("filled");
+  //   }
+  // });
+  // burger.addEventListener("click", () => {
+  //   mobileMenu.classList.toggle("active");
+  // });
 
   function initMap() {
     new google.maps.Map(document.getElementById("map"), {
--- projects/cbd/src/js/app.js
+++ projects/cbd/src/js/app.js
@@ -6,7 +6,9 @@ const mobileMenu = document.getElementById("mobileNav");
 document.addEventListener("scroll", () => {
   if (window.pageYOffset < navBar.clientHeight) {
     navBar.classList.remove("filled");
+    burger.classList.remove("filled");
   } else {
     navBar.classList.add("filled");
+    burger.classList.add("filled");
   }
 });
